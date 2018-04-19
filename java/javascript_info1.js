e31e434540cab72ff84b2137928a1372f1f5147d
reinaoyo <32255176+reinaoyo@users.noreply.github.com>
2018-04-19 22:25:08 +0900

    Add files via upload

---
+++ java/javascript_info1.js
@@ -0,0 +1,84 @@
+
+
+$(function() {
+  $('.a').click(function(){
+      $('.info1').css('display','block');
+  });
+});
+$(function(){
+    $('.a').click(function(){
+       $(this).css('z-index','1');
+    })
+});
+$(function() {
+  $('.b').click(function(){
+      $('.info2').css('display','block');
+  });
+});
+$(function(){
+  $('.b').click(function(){
+ $(this).css('z-index','1');
+    })
+});
+$(function() {
+  $('.c').click(function(){
+      $('.info3').css('display','block');
+  });
+});
+$(function(){
+    $('.c').click(function(){
+       $(this).css('z-index','1');
+    })
+});
+
+$(function() {
+  $('.info_a').click(function(){
+      $('.nakama').css('display','block');
+  });
+});
+
+$(function() {
+  $('.info_b').click(function(){
+      $('.nakama').css('display','block');
+  });
+});
+
+$(function() {
+  $('.info_c').click(function(){
+      $('.nakama').css('display','block');
+  });
+});
+
+
+$(function() {
+  $('.next1').click(function(){
+      $('.info1').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.info2').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.info3').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.nakama').css('display','none');
+  });
+});
+
+
+/*$(function() {
+  $('.next1').click(function(){
+      $('.nakama_b').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.nakama_c').css('display','none');
+  });
+});*/
---
+++ java/javascript_info2.js
@@ -0,0 +1,30 @@
+$(function() {
+  $('.info_a').click(function(){
+      $('.na').css('display','block');
+  });
+});
+$(function() {
+  $('.info_b').click(function(){
+      $('.nb').css('display','block');
+  });
+});
+$(function() {
+  $('.info_c').click(function(){
+      $('.nc').css('display','block');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.na').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.nb').css('display','none');
+  });
+});
+$(function() {
+  $('.next1').click(function(){
+      $('.nc').css('display','none');
+  });
+});
